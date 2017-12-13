eb44ba3c0930678e4d88c39419c67677cce9d6c5
Reginaldo Alves de Barros <reginaldo.barros@fcamara.com.br>
2017-12-13 14:19:11 -0200

    initial commit

---
+++ Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.IO;
+
+namespace ReadFiles
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string directory = Directory.GetCurrentDirectory();
+            string filePath = Path.Combine(directory, "Myfiles/template.html");
+
+            string template = File.ReadAllText(filePath);
+
+            Console.WriteLine(template);
+        }
+    }
+}
